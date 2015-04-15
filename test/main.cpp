aae4a93c8ff083d0dec055e4ee91ab047406c657
田子桐 <Ricardo@tianzitongdeMacBook-Pro.local>
2015-04-15 19:57:56 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 田子桐 on 15/4/15.
+//  Copyright (c) 2015年 田子桐. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
